6001c02a5fbf3b108914aef71d0fb47733dabd58
Izza Fatima <114181291+izzaa04@users.noreply.github.com>
2023-05-31 23:40:25 +0400

    changes

--- try.js
+++
@@ -1,9 +0,0 @@
-// Changing the text dynamically
-const scrollingText = document.querySelector(".scrolling-text");
-scrollingText.textContent = "New text dynamically changed!";
-
-// Pausing the scrolling animation
-scrollingText.style.animationPlayState = "paused";
-
-// Resuming the scrolling animation
-scrollingText.style.animationPlayState = "running";
